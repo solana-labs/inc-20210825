f8ef14abc98d04b9721fc179a8f606c06f0a8460
Ryo Onodera <ryoqun@gmail.com>
2021-08-26 14:44:41 +0900

    Minor rename

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,7 +6,7 @@ SPL_TOKEN=${SPL_TOKEN:-../solana-program-library/target/release/spl-token}
 SPL_KEYGEN=${SPL_KEYGEN:-../solana/target/release/solana-keygen}
 
 MINT=$1
-TARGET_ACCOUNT=$2
+TARGET_OWNER=$2
 COMMON=${COMMON:-"--verbose -ul"}
 
 BAD_ACCOUNT=$($SPL_KEYGEN grind --starts-with bad:1 --ignore-case | tail -n1 | awk '{print $4}')
@@ -17,5 +17,5 @@ $SPL_TOKEN $COMMON mint "$MINT" 10 "$BAD_ACCOUNT"
 $SPL_TOKEN $COMMON account-info "$BAD_ACCOUNT"
 $SPL_TOKEN $COMMON approve "$BAD_ACCOUNT" 999 "$BAD_ACCOUNT"
 $SPL_TOKEN $COMMON account-info "$BAD_ACCOUNT"
-$SPL_TOKEN $COMMON authorize "$BAD_ACCOUNT" owner "$TARGET_ACCOUNT"
+$SPL_TOKEN $COMMON authorize "$BAD_ACCOUNT" owner "$TARGET_OWNER"
 $SPL_TOKEN $COMMON account-info "$BAD_ACCOUNT"
